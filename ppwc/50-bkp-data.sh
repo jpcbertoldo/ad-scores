fd4b06fd463e7ad7204e5cce06999c71a38c5086
jpcbertoldo <24547377+jpcbertoldo@users.noreply.github.com>
2022-08-18 16:17:03 +0200

    correct bkp script

--- ppwc/50-bkp-data.sh
+++ ppwc/50-bkp-data.sh
@@ -5,16 +5,19 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRIPT_DIR=$(realpath $SCRIPT_DIR)
 echo "[debug] SCRIPT_DIR=${SCRIPT_DIR}"
 
+cd ${SCRIPT_DIR}
+echo "[debug] pwd=$(pwd)"
+
 DATA_LINKS_FPATH="${SCRIPT_DIR}/00-data-links.txt"
 echo "[debug] DATA_LINKS_FPATH=${DATA_LINKS_FPATH}"
 
-DATA_DIR=${SCRIPT_DIR}/data
+DATA_DIR=./data
 echo "[debug] DATA_DIR=${DATA_DIR}"
 
 TODAY=$(date +'%Y-%m-%d')
 echo "[debug] TODAY=${TODAY}"
 
-BKP_DIR=${SCRIPT_DIR}/bkp
+BKP_DIR=./bkp
 echo "[debug] BKP_DIR=${BKP_DIR}"
 
 BKP_ZIP_FNAME=${TODAY}-all-data.zip
@@ -30,6 +33,7 @@ if [ -f ${BKP_DIR}/${BKP_ZIP_FNAME} ]; then
     exit 0
 fi
 
-zip -r ${BKP_DIR}/${BKP_ZIP_FNAME} ${DATA_DIR}/*.json -x ${EXCLUDED_FILES}
+zip -r ${BKP_DIR}/${BKP_ZIP_FNAME} ${DATA_DIR} -x ${EXCLUDED_FILES}
+# zip -r ${BKP_DIR}/${BKP_ZIP_FNAME} ${DATA_DIR}/*.svg -x ${EXCLUDED_FILES}
 
-ls -lh ${BKP_DIR} | tail -n 
+ls -lh ${BKP_DIR} | tail -n 5
